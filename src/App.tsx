e6f2a0b47bf36872e6af905456d50a4c74a692fc
Azu0925 <yamaneko564783@gmail.com>
2022-11-01 12:04:04 +0900

    style* code format

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { Textarea } from '@chakra-ui/react'
-import './App.css';
+import './App.css'
 
 function App() {
-    const [jsonText, setJsonText] = useState("")
-    const handleChangeInputText = (e: React.ChangeEvent<HTMLTextAreaElement>) => setJsonText(JSON.stringify(JSON.parse(e.target.value), null, 2))
+  const [jsonText, setJsonText] = useState('')
+  const handleChangeInputText = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setJsonText(JSON.stringify(JSON.parse(e.target.value), null, 2))
   return (
     <div className="App">
-        <div>
-            <form>
-                <Textarea onChange={handleChangeInputText} />
-            </form>
-        </div>
-        <div>
-            <form>
-
-                <Textarea value={jsonText} />
-            </form>
-        </div>
+      <div>
+        <form>
+          <Textarea onChange={handleChangeInputText} />
+        </form>
+      </div>
+      <div>
+        <form>
+          <Textarea value={jsonText} />
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
