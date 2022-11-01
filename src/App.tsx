1eed6047530907b73d3c0c6aafb00352343186f5
Azu0925 <yamaneko564783@gmail.com>
2022-11-01 23:40:19 +0900

    feature: ベータ版

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react'
-import { Textarea } from '@chakra-ui/react'
-import './App.css'
+import { Box, Flex, Textarea, Text } from '@chakra-ui/react'
+import { Header } from './components/headers/Header'
 
 function App() {
   const [jsonText, setJsonText] = useState('')
   const handleChangeInputText = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
     setJsonText(JSON.stringify(JSON.parse(e.target.value), null, 2))
+
   return (
     <div className="App">
-      <div>
-        <form>
-          <Textarea onChange={handleChangeInputText} />
-        </form>
-      </div>
-      <div>
-        <Textarea value={jsonText} readOnly />
-      </div>
+      <Header />
+      <Box w="80vw" p="5vh 5vw" m="0 auto" rounded="10" bgColor="white">
+        <Flex w="100%">
+          <Box w="50%" mr="10px">
+            <Text>Input JSON</Text>
+            <Textarea onChange={handleChangeInputText} w="100%" h="70vh" placeholder="Input your json" resize="none" />
+          </Box>
+          <Box w="50%">
+            <Text>Output formatted JSON</Text>
+            <Textarea value={jsonText} w="100%" h="70vh" placeholder="Result" resize="none" readOnly />
+          </Box>
+        </Flex>
+      </Box>
     </div>
   )
 }
---
+++ src/components/headers/Header.tsx
@@ -0,0 +1,15 @@
+import { Box, HStack, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export const Header = () => {
+  return <Box h="10vh"></Box>
+  /*return (
+    <Box h="10vh">
+      <HStack h="100%" ml="10px">
+        <Text color="gray.200" fontSize="large">
+          JSON shaping
+        </Text>
+      </HStack>
+    </Box>
+  )*/
+}
